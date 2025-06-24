dfeb0e2a62a7037a0a2f9b1896f7dde8bd182126
Zhdanov <zhdanov190@mail.ru>
2025-06-24 15:37:46 +1000

    Добавлены новые теги

--- backend/recipes/management/commands/load_tags.py
+++ backend/recipes/management/commands/load_tags.py
@@ -10,6 +10,10 @@ class Command(BaseCommand):
             {'name': 'Завтрак', 'color': '#E26C2D', 'slug': 'breakfast'},
             {'name': 'Обед', 'color': '#49B64E', 'slug': 'lunch'},
             {'name': 'Ужин', 'color': '#8775D2', 'slug': 'dinner'},
+            {'name': 'Напитки', 'color': '#FF6B6B', 'slug': 'drinks'},
+            {'name': 'Закуски', 'color': '#4ECDC4', 'slug': 'appetizers'},
+            {'name': 'Мороженое', 'color': '#45B7D1', 'slug': 'ice-cream'},
+            {'name': 'Сладости', 'color': '#FFA07A', 'slug': 'desserts'},
         ]
         Tag.objects.bulk_create(
             [Tag(**tag) for tag in data],
