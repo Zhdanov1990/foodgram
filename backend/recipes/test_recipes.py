6f6276a27a523422fac2205afccc98b98916a863
Zhdanov <zhdanov190@mail.ru>
2025-06-25 01:50:26 +1000

    проверка автоматических тестов

--- backend/recipes/test_recipes.py
+++ backend/recipes/test_recipes.py
@@ -1,13 +1,18 @@
 import pytest
 from django.urls import reverse
 from rest_framework.test import APIClient
+
 from recipes.models import Tag, Ingredient, Recipe, RecipeIngredient
 from users.models import User
 
 
 @pytest.mark.django_db
 def test_tag_creation():
-    tag = Tag.objects.create(name="Тест", color="#FFFFFF", slug="test")
+    tag = Tag.objects.create(
+        name="Тест",
+        color="#FFFFFF",
+        slug="test"
+    )
     assert tag.name == "Тест"
     assert tag.color == "#FFFFFF"
     assert tag.slug == "test"
@@ -15,16 +20,29 @@ def test_tag_creation():
 
 @pytest.mark.django_db
 def test_ingredient_creation():
-    ingr = Ingredient.objects.create(name="Картофель", measurement_unit="г")
+    ingr = Ingredient.objects.create(
+        name="Картофель",
+        measurement_unit="г"
+    )
     assert ingr.name == "Картофель"
     assert ingr.measurement_unit == "г"
 
 
 @pytest.mark.django_db
 def test_recipe_creation():
-    user = User.objects.create_user(username="testuser", password="testpass")
-    ingr = Ingredient.objects.create(name="Морковь", measurement_unit="г")
-    tag = Tag.objects.create(name="Обед", color="#49B64E", slug="lunch")
+    user = User.objects.create_user(
+        username="testuser",
+        password="testpass"
+    )
+    ingr = Ingredient.objects.create(
+        name="Морковь",
+        measurement_unit="г"
+    )
+    tag = Tag.objects.create(
+        name="Обед",
+        color="#49B64E",
+        slug="lunch"
+    )
     recipe = Recipe.objects.create(
         author=user,
         name="Тестовый рецепт",
@@ -32,7 +50,11 @@ def test_recipe_creation():
         text="Описание",
         cooking_time=10
     )
-    RecipeIngredient.objects.create(recipe=recipe, ingredient=ingr, amount=100)
+    RecipeIngredient.objects.create(
+        recipe=recipe,
+        ingredient=ingr,
+        amount=100
+    )
     recipe.tags.add(tag)
     assert recipe.name == "Тестовый рецепт"
     assert recipe.author == user
@@ -42,19 +64,22 @@ def test_recipe_creation():
 
 @pytest.mark.django_db
 def test_recipe_str():
-    recipe = Recipe(name='Тестовый рецепт')
-    assert str(recipe) == 'Тестовый рецепт'
+    recipe = Recipe(name="Тестовый рецепт")
+    assert str(recipe) == "Тестовый рецепт"
 
 
 @pytest.mark.django_db
 def test_ingredient_str():
-    ingredient = Ingredient(name='Сахар', measurement_unit='г')
-    assert str(ingredient) == 'Сахар (г)'
+    ingredient = Ingredient(
+        name="Сахар",
+        measurement_unit="г"
+    )
+    assert str(ingredient) == "Сахар (г)"
 
 
 @pytest.mark.django_db
 def test_recipe_list_api():
     client = APIClient()
-    url = reverse('recipe-list')
+    url = reverse("recipes-list")
     response = client.get(url)
     assert response.status_code == 200
