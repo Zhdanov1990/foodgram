2f287de4398dda6db300d62696cf56da60c8744c
Zhdanov <zhdanov190@mail.ru>
2025-06-30 13:28:09 +1000

    Исправлен urls.py

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -4,23 +4,6 @@ from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path
 
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Foodgram API",
-        default_version='v1',
-        description="API для проекта Foodgram",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 
 def root_view(request):
     return JsonResponse({"status": "API is running"})
@@ -31,11 +14,6 @@ urlpatterns = [
     path('api/', include('api.urls', namespace='api')),
     path('api/auth/', include('djoser.urls.authtoken')),
     path('api-auth/', include('rest_framework.urls')),
-    path(
-        'api/docs/',
-        schema_view.with_ui('swagger', cache_timeout=0),
-        name='schema-swagger-ui'
-    ),
     path('', root_view),
 ]
 
